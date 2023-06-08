d4e125b23f678d79365c3826fec811dc4af3e062
Marie <108511871+Mary-Wanyama@users.noreply.github.com>
2023-06-08 19:52:25 +0300

    Update catsSlice.js

--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,3 +1,31 @@
-export function fetchCats() {}
-
-export default function catsReducer() {}
+export function fetchCats() {
+return{
+  dispatch({type: "action/catLoading"})
+  fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+  .then(res=> res.json())
+  .then(data =>{
+    dispatch({
+    type: "action/catLoaded", payload: data.images
+    })
+  })
+}
+const initialState = {
+  entities: [],
+  status: "idle"
+}
+export default function catsReducer(state = initialState, action) {
+switch(action.type){
+  case "action/catLoading":
+    return{
+      ...state,
+      status: 'loading...'
+    }
+   case 'action/catLoaded'
+    return{
+     ...state,
+        entities: action.payload,
+        status: "idle",
+}
+  default: 
+  return state
+}
