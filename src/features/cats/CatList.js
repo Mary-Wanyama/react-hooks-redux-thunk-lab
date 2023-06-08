b8c7a245324a41eb7620edad689261561ef95e9f
Marie <108511871+Mary-Wanyama@users.noreply.github.com>
2023-06-08 20:07:38 +0300

    Update CatList.js

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,15 @@
 // write your CatList component here
+
+import React from 'react'
+import useSelector from 'react-redux'
+
+export default function List(){
+  const listing = Useselector(state=>state.cats.entities)
+return(
+<ul>
+  {listing.map(list)=>{
+<li key={list.id}>{{list.image} {list.name}</li>
+}
+  </ul>
+)
+}
