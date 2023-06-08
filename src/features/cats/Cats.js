3437a5eecc2e78298a5dee3924f14a14f22357ef
Marie <108511871+Mary-Wanyama@users.noreply.github.com>
2023-06-08 20:01:52 +0300

    Update Cats.js

--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,10 +1,23 @@
 import React from "react";
+import {useSelector, useDispatch} from 'react-redux'
 
 function Cats() {
+  
+  const catsPics = useSelector(state => state.entities)
+  const dispatch = useDispatch()
+  
+    useEffect(() => {
+    dispatch(fetchCats());
+  }, []);
   return (
     <div>
       <h1>CatBook</h1>
-      {/* add CatList component here */}
+    <ul>
+    {catsPics.map(cats)=>{
+  <li key={cats.id}>{cats.image}{cats.name}</li>
+  }
+    </ul>
+      
     </div>
   );
 }
