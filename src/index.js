07c08c3dbf0e69f3d1a1217329657527f3ffd7cf
Marie <108511871+Mary-Wanyama@users.noreply.github.com>
2023-06-08 19:15:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux"
-import { createStore} from "redux"
+import { createStore, applyMiddleware} from "redux"
+import {thunk} from "redux-thunk"
+import {composeWithDevTools} from "redux-devtools-extension"
 import "bootstrap/dist/css/bootstrap.min.css";
 import catsReducer from './features/cats/catsSlice.js'
 import App fron "./App"
 
-const store = createStore(catsReducer)
+const store = createStore(catsReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
